e0b9dd77ec8a36fcf29d70d96b6b44409cc5e1da
Victor Olabanji <54726522+Vickthour@users.noreply.github.com>
2023-01-23 09:40:33 -0500

    Update Portfolio.jsx

--- src/components/pages/Portfolio/Portfolio.jsx
+++ src/components/pages/Portfolio/Portfolio.jsx
@@ -1,42 +1,39 @@
 import React from "react";
-import image1 from "../../../images/portfolio/project-01.jpg";
-import image2 from "../../../images/portfolio/project-02.jpg";
-import image3 from "../../../images/portfolio/project-03.jpg";
+import image1 from "../../../images/portfolio/sadiq.jpg";
+import image2 from "../../../images/portfolio/crypto.jpg";
+import image3 from "../../../images/portfolio/wield.jpg";
 import image4 from "../../../images/portfolio/project-04.jpg";
 import PortfolioItem from "./PortfolioItem";
 
 const portfolioData = [
-  // {
-  //   id: 1,
-  //   image: image1,
-  //   title: "Space Bootstrap 5 Multipurpose Template",
-  //   link: "#0",
-  //   description:
-  //     "",
-  // },
-  // {
-  //   id: 2,
-  //   image: image2,
-  //   title: "eCommerce HTML Bootstrap 5 UI Kit",
-  //   link: "#0",
-  //   description:
-  //     "",
-  // },
+  {
+    id: 1,
+    image: image1,
+    title: "HTML & CSS website",
+    link: "https://sadiqisu.netlify.app/",
+    description:"A personal website",
+  },
+  {
+    id: 2,
+    image: image2,
+    title: "Cryptoverse",
+    link: "https://crypto-analyze.netlify.app/",
+    description:
+      "Cryptocurrency Analysis Application. Offers a nice range of cryptocurrencies and a simple-to-use app experience",
+  },
   {
     id: 3,
     image: image3,
-    title: "Basic – Tailwind CSS freestyle",
-    link: "#0",
-    description:
-      "",
+    title: "wield frontend (React)",
+    link: "https://wield-frontend-pi.vercel.app/",
+    description:"",
   },
   {
     id: 4,
     image: image4,
     title: "Gearix Car repair App",
     link: "#0",
-    description:
-      "",
+    description:"",
   },
 ];
 
